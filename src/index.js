5da4d5edd7c6bee89e79f0667a679a7127998b40
mikiteek <mikiteek@gmail.com>
2020-08-04 18:21:52 +0300

    switch-colors -> separate repo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+import colors from "./js/colors";
 import './sass/styles.scss';
+
+const refs = {
+  body: document.querySelector("body"),
+  start: document.querySelector("button[data-action='start']"),
+  stop: document.querySelector("button[data-action='stop']"),
+}
+let statusChangeTheme = false;
+const TIME_OUT = 1000;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+refs.start.addEventListener("click", startChangeIntervalTheme);
+refs.stop.addEventListener("click", stopChangeTheme);
+
+function setTheme() {
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+function startChangeIntervalTheme() {
+  if (statusChangeTheme) {
+    return;
+  }
+  statusChangeTheme = setInterval(setTheme, TIME_OUT);
+}
+function stopChangeTheme() {
+  if (!statusChangeTheme) {
+    return;
+  }
+  clearInterval(statusChangeTheme);
+  statusChangeTheme = false;
+}
